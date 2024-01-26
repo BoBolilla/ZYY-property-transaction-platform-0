6789d63096c9cf7a91fd010a1d6e2707e30a3e36
BoBolilla <1326511780@qq.com>
2024-01-26 16:12:30 +0800

    测试git

---
+++ src/stores/index.js
@@ -0,0 +1,9 @@
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+export default pinia
+
+export * from './modules/user' //导出user里面的所有按需导出

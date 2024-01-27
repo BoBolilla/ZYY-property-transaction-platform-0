b926c315aa54d8298e46fcc803a98a04cead4711
BoBolilla <1326511780@qq.com>
2024-01-27 22:01:02 +0800

    导航栏

--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,8 +1,98 @@
-<script setup></script>
+<script setup>
+import { Search } from '@element-plus/icons-vue'
+</script>
 
 <template>
-  <div>我是首页架子</div>
-  <div><router-view></router-view></div>
+  <div class="layout-container">
+    <el-container>
+      <el-header class="container-top" height="70px">
+        <el-menu
+          :default-active="$route.path"
+          class="el-menu"
+          mode="horizontal"
+          :ellipsis="false"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          @select="handleSelect"
+          router
+        >
+          <div class="flex-grow" />
+          <el-menu-item index="/home">首页</el-menu-item>
+          <el-sub-menu index="/news">
+            <template #title>新闻</template>
+            <el-menu-item index="/news/current">时事新闻</el-menu-item>
+            <el-menu-item index="/news/work">工作状态</el-menu-item>
+            <el-menu-item index="/news/media">媒体发布</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index=" /organization">
+            <template #title>机构</template>
+            <el-menu-item index="/organization/history">历史</el-menu-item>
+            <el-menu-item index="/organization/capital">注册资本</el-menu-item>
+            <el-menu-item index="/organization/culture">文化</el-menu-item>
+            <el-menu-item index="/organization/philosophy">宗旨</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="/feedback">
+            <template #title>反馈</template>
+            <el-menu-item index="/feedback/submit">意见提交</el-menu-item>
+            <el-menu-item index="/feedback/release">处理公示</el-menu-item>
+          </el-sub-menu>
+          <div class="flex-grow1" />
+
+          <div class="logo">
+            <img
+              style="width: 190px"
+              src="../../assets/ZYY-logo.png"
+              alt="logo"
+            />
+          </div>
+
+          <div class="flex-grow2" />
+          <el-menu-item index="/transaction">交易大厅</el-menu-item>
+          <div class="search">
+            <el-input
+              v-model="input3"
+              placeholder="Please input"
+              class="input-with-select"
+            >
+              <template #append>
+                <el-button :icon="Search" />
+              </template>
+            </el-input>
+          </div>
+        </el-menu>
+      </el-header>
+      <!-- 顶栏 -->
+      <el-main
+        ><div><router-view></router-view></div
+      ></el-main>
+      <!-- 二级路由出口 -->
+      <el-footer class="container-bottom">Footer</el-footer>
+      <!-- 底栏 -->
+    </el-container>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.search {
+  margin: 13px;
+}
+.layout-container {
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.container-top {
+  margin: 0;
+  padding: 0;
+}
+
+.logo {
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+</style>

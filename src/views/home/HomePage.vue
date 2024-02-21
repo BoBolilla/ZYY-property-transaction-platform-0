9487d3725bdba74454a6308cc3cbde066b96087a
BoBolilla <1326511780@qq.com>
2024-02-21 21:44:38 +0800

    愿景使命样式2.21

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -11,19 +11,58 @@ import HomeCooperation from './components/HomeCooperation.vue'
   <div class="container">
     <HomeBanner />
   </div>
-  <el-row>
-    <el-col :span="2"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="20"
-      ><div class="grid-content ep-bg-purple" />
+  <div class="show">
+    <div class="yj">
+      <img src="@/assets/images/yj.png" />
+      <h3>愿景</h3>
+      <p>
+        以优质服务赢得企业满意，以优秀业绩赢得行 业领先
+        引领着创业幼苗沐浴党和政府的阳光，承载时 代重任.
+      </p>
+    </div>
+    <div class="shim">
+      <img src="@/assets/images/shim.png" />
+      <h3>使命</h3>
+      <p>
+        发展高科技，促进产业化 通过“联络员+辅导员+创业导师"的三级辅导
+        体系和十多个专业服务平台。
+      </p>
+    </div>
+  </div>
+  <HomeAchievement />
+  <HomeNews />
 
-      <HomeAchievement />
-      <HomeNews />
-
-      <HomeOrganization />
-      <HomeCooperation />
-      <HomeWork />
-      <img src="@\assets\images\bottom.png" alt="" />
-    </el-col>
-    <el-col :span="2"><div class="grid-content ep-bg-purple" /></el-col>
-  </el-row>
+  <HomeOrganization />
+  <HomeCooperation />
+  <HomeWork />
+  <img src="@\assets\images\bottom.png" alt="" />
 </template>
+
+<style scoped>
+.show {
+  position: absolute;
+  top: 510px;
+  left: 821px;
+  width: 500px;
+  height: 250px;
+  color: white;
+}
+.show img {
+  margin: 10px;
+  margin-top: 30px;
+}
+.show .yj {
+  padding: 20px;
+  float: left;
+  width: 250px;
+  height: 250px;
+  background-color: #235db6;
+}
+.show .shim {
+  padding: 20px;
+  float: left;
+  width: 250px;
+  height: 250px;
+  background-color: #e46841;
+}
+</style>
